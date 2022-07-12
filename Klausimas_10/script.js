40b781fbd141d75dab7c328d4f9625d2f4236ff8
Andrius M <andrius.matuliauskas@gmail.com>
2022-07-12 22:09:03 +0300

    task 10

--- Klausimas_10/script.js
+++ Klausimas_10/script.js
@@ -1,10 +1,16 @@
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
+import {composition} from '../Klausimas_10/modules/math/composition.js';
+import {division} from '../Klausimas_10/modules/math/division.js';
+import {subtraction} from '../Klausimas_10/modules/math/subtraction.js';
+import {multiplication} from '../Klausimas_10/modules/math/multiplication.js';
+
+import {one, two, three, four, five} from '../Klausimas_10/modules/numbers/numbers.js';
 
 let a = composition(one, four);
 let b = division(four, two);
-let c = substraction(three, two);
+let c = subtraction(three, two);
 let d = multiplication(five, two);
 
 console.log(a);
