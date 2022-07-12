488471a047c3eeccf500775ddd6a2014630d49e9
Andrius M <andrius.matuliauskas@gmail.com>
2022-07-12 21:25:37 +0300

    task 4

--- Klausimas_4/script.js
+++ Klausimas_4/script.js
@@ -10,3 +10,23 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+window.addEventListener('load', 
+function fetchPosts(){
+  fetch(ENDPOINT)
+  .then((res) => res.json())
+  .then((data) => {
+    let output = document.getElementById('#output');
+    data.forEach((car) => {
+       output += `
+       <div class="car-card">
+          <h3 class="car-brand">${car.brand}</h3>
+          ${'<ul>' + car.models.map(function (model) {
+            return '<li>' + model + '</li>';
+        }).join('') + '</ul>'}
+       </div>
+       `
+    });
+    document.querySelector('#output').innerHTML = output;
+  })
+});
